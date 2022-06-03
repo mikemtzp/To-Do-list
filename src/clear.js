de1f181cfdeadc13a05056f76248aca433f34fb8
mikemtzp <mikemp8226@gmail.com>
2022-06-03 17:06:17 -0500

    Add interactive list

---
+++ src/clear.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+import { setStorage } from './storage.js';
+import { deleteTask } from './crud-methods.js';
+
+export { clearAll };
+
+const clearAll = () => {
+  const localData = JSON.parse(localStorage.getItem('todo'));
+  const taskContainer = document.querySelectorAll('.list-item');
+  taskContainer.forEach((e) => {
+    if (e.classList.contains('list-selected-item')) {
+      deleteTask(e);
+    }
+  });
+  let count = 0;
+  const data = Array.from(localData).filter((e) => e.completed === false);
+  data.map((e) => e.index = count += 1);
+  setStorage(data);
+};
